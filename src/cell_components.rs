9c696a3cea246edf1aad227eb677030568e72804
Christopher Lang <christopher-d-lang@hotmail.com>
2020-10-08 16:39:52 +0100

    Revert project to an empty one

--- src/cell_components.rs
+++
@@ -1,8 +0,0 @@
-mod sand;
-pub use sand::Sand;
-
-use crate::cell_storage::InnerCellStorage;
-
-pub trait CellComponent: Sized + Default + Clone + Copy + 'static {
-    type CellStorage: InnerCellStorage<Self>;
-}
--- src/object_systems.rs
+++
@@ -1,3 +0,0 @@
-mod print_position;
-
-pub use print_position::PrintPosition;
