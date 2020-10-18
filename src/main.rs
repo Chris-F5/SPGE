ea68e254a882929f1e4afed27c97b6b45ad74f54
Christopher Lang <christopher-d-lang@hotmail.com>
2020-10-18 10:48:43 +0100

    Create window and draw demo texture with crow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use crow::{
+    glutin::{
+        event::{Event, WindowEvent},
+        event_loop::{ControlFlow, EventLoop},
+        window::WindowBuilder,
+    },
+    Context, DrawConfig, Texture,
+};
 use shred::{DispatcherBuilder, SystemData, World};
 use spge::{
     components::cell_components::{CellColor, TestComp},
@@ -5,7 +13,8 @@ use spge::{
     systems::DrawSystem,
 };
 
-fn main() {
+fn main() -> Result<(), crow::Error> {
+    // INIT world
     let mut world = World::empty();
 
     let cell_colors: MaskedCellStorage<CellColor> = Default::default();
@@ -25,4 +34,29 @@ fn main() {
         .with(DrawSystem, "test_system", &[])
         .build();
     dispatcher.dispatch(&mut world);
+
+    // INIT crow window and context
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().with_title("SPGE Test");
+    let mut ctx = Context::new(window, &event_loop)?;
+
+    let texture = Texture::load(&mut ctx, "./test_texture.png")?;
+
+    // Run crow event loop
+    event_loop.run(
+        move |event: Event<()>, _window_target: _, control_flow: &mut ControlFlow| match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            Event::MainEventsCleared => ctx.window().request_redraw(),
+            Event::RedrawRequested(_) => {
+                let mut surface = ctx.surface();
+                ctx.clear_color(&mut surface, (0.4, 0.4, 0.8, 1.0));
+                ctx.draw(&mut surface, &texture, (100, 150), &DrawConfig::default());
+                ctx.present(surface).unwrap();
+            }
+            _ => (),
+        },
+    )
 }
